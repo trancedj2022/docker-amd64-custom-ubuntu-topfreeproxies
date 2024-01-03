675758b75b92db25877a33ebc02b8f209ad2200e
刘栋19950828 <144557489+smallflowercat1995@users.noreply.github.com>
2024-01-04 04:28:15 +0800

    Update start.sh
    去除sudo

--- debian.backup/topfreeproxies/start.sh
+++ debian.backup/topfreeproxies/start.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 bash ./utils/scripts/set_proxy.sh
-sudo chmod +x ./utils/subconverter/subconverter ; sudo chmod +x ./utils/litespeedtest/lite
+chmod -v +x ./utils/subconverter/subconverter ./utils/litespeedtest/lite
 python ./utils/main.py
 sed -i 's;port: 7890;port: '${HTTP_PORT}';g;s;socks-port: 7891;socks-port: '${SOCKS5_PORT}';g;s;external-controller: :9090;external-controller: :7896;g' ./Eternity.yaml
