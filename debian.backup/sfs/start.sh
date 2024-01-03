76f5ccc397b081da1785e63dda7cd2438205449c
刘栋19950828 <144557489+smallflowercat1995@users.noreply.github.com>
2024-01-04 04:33:24 +0800

    Update start.sh

--- debian.backup/sfs/start.sh
+++ debian.backup/sfs/start.sh
@@ -3,11 +3,8 @@ clear
 kill -9 $(ps -ef | grep -v grep | grep linux-amd64 | awk '{print $2}')
 if [ "$?" == "0"  ];then echo yes,i get linux-amd64 clash and kill it;else echo oh not get;fi
 
-PWD=`pwd`/sources
-cd ${PWD}
+cd $(pwd)/sources
 rm -rfv *.log
 cp -fv Country.mmdb .config/
-
 cp -fv "../../topfreeproxies/Eternity.yaml" ".config/config.yaml"
-chmod u+x linux-amd64
 nohup ./linux-amd64 -f .config/config.yaml -d .config > linux-amd64.log 2>&1 & disown
