ab0913a72fdfd6a9b454fca535a96fdc32103675
刘栋19950828 <144557489+smallflowercat1995@users.noreply.github.com>
2024-01-04 04:17:09 +0800

    Update set_proxy.sh
    在修改以下

--- debian.backup/topfreeproxies/utils/scripts/set_proxy.sh
+++ debian.backup/topfreeproxies/utils/scripts/set_proxy.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 clear
 kill -9 $(ps -ef | grep -v grep | grep mihomos | awk '{print $2}')
 if [ "$?" == "0"  ];then echo yes,i get mihomos clash and kill it;else echo oh not get;fi
@@ -10,14 +9,20 @@ if [ $(uname -m) = 'x86_64' ];then
     echo "is amd64"
     #version=$(curl -s "https://api.github.com/repos/Dreamacro/clash/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
     version=$(curl -s "https://api.github.com/repos/MetaCubeX/mihomo/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-    #wget --verbose --show-progress=on --progress=bar --hsts-file=/tmp/wget-hsts --continue --retry-connrefused --waitretry=1 --timeout=30 --tries=3 "https://github.com/Dreamacro/clash/releases/download/$version/clash-linux-amd64-$version.gz" -O clash.gz
-    wget --verbose --show-progress=on --progress=bar --hsts-file=/tmp/wget-hsts --continue --retry-connrefused --waitretry=1 --timeout=30 --tries=3 "https://github.com/MetaCubeX/mihomo/releases/download/$version/mihomo-linux-amd64-$version.gz" -O mihomo.gz
+    #wget --verbose --show-progress=on --progress=bar --hsts-file=/tmp/wget-hsts --continue --retry-connrefused --waitretry=1 --timeout=30 --tries=3 "https://github.com/Dreamacro/clash/releases/down>
+    wget --verbose --show-progress=on --progress=bar --hsts-file=/tmp/wget-hsts --continue --retry-connrefused --waitretry=1 --timeout=30 --tries=3 "https://github.com/MetaCubeX/mihomo/releases/down>
 else
     echo "is not x86_64"
 fi
 unset http_proxy https_proxy all_proxy HTTP_PROXY HTTPS_PROXY ALL_PROXY
+
 gunzip -f mihomo.gz
+
 mv -fv ./mihomo ./mihomos
+cp -fv ./mihomos ../sfs/sources/linux-amd64
+chmod -v +x ./mihomos ../sfs/sources/linux-amd64
+rm -fv ./mihomo.gz ./mihomo
+
 # Initialize Mihomo
 sudo chmod +x ./mihomos && ./mihomos &
 
