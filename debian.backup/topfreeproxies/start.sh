83af0fe610e4fc5ac67ac59b56b446058c26e02c
刘栋19950828 <144557489+smallflowercat1995@users.noreply.github.com>
2024-01-04 06:28:51 +0800

    Update start.sh

--- debian.backup/topfreeproxies/start.sh
+++ debian.backup/topfreeproxies/start.sh
@@ -2,4 +2,4 @@
 bash ./utils/scripts/set_proxy.sh
 chmod -v +x ./utils/subconverter/subconverter ./utils/litespeedtest/lite
 python ./utils/main.py
-sed -i 's;port: 7890;port: '${HTTP_PORT}';g;s;socks-port: 7891;socks-port: '${SOCKS5_PORT}';g;s;external-controller: :9090;external-controller: :7896;g' ./Eternity.yaml
+sed -i 's;port: 7890;port: 7891;g;s;socks-port: 7891;socks-port: 7892;g;s;external-controller: :9090;external-controller: :7896;g' ./Eternity.yaml
