240cee7810ee5cd885f5772273517f417a80b200
刘栋19950828 <144557489+smallflowercat1995@users.noreply.github.com>
2024-01-04 04:26:25 +0800

    Update set_proxy.sh
    补充sudo命令

--- debian.backup/topfreeproxies/utils/scripts/set_proxy.sh
+++ debian.backup/topfreeproxies/utils/scripts/set_proxy.sh
@@ -1,16 +1,19 @@
+#!/usr/bin/env bash
 clear
-kill -9 $(ps -ef | grep -v grep | grep mihomos | awk '{print $2}')
-if [ "$?" == "0"  ];then echo yes,i get mihomos clash and kill it;else echo oh not get;fi
+#sudo kill -9 $(ps -ef | grep -v grep | grep mihomos | awk '{print $2}')
+sudo pkill -f mihomos
+if [ "$?" == "0"  ];then echo yes,i get mihomos and kill it;else echo oh not get;fi
 
 export IP=127.0.0.1 H_P=7891 S_P=7892
 export http_proxy=http://${IP}:${H_P} https_proxy=http://${IP}:${H_P} all_proxy=socks5://${IP}:${S_P} HTTP_PROXY=http://${IP}:${H_P} HTTPS_PROXY=http://${IP}:${H_P} ALL_PROXY=socks5://${IP}:${S_P}
+curl -svv www.github.com
 # Download mihomo
 if [ $(uname -m) = 'x86_64' ];then
     echo "is amd64"
-    #version=$(curl -s "https://api.github.com/repos/Dreamacro/clash/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-    version=$(curl -s "https://api.github.com/repos/MetaCubeX/mihomo/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-    #wget --verbose --show-progress=on --progress=bar --hsts-file=/tmp/wget-hsts --continue --retry-connrefused --waitretry=1 --timeout=30 --tries=3 "https://github.com/Dreamacro/clash/releases/down>
-    wget --verbose --show-progress=on --progress=bar --hsts-file=/tmp/wget-hsts --continue --retry-connrefused --waitretry=1 --timeout=30 --tries=3 "https://github.com/MetaCubeX/mihomo/releases/down>
+    #sudo version=$(curl -s "https://api.github.com/repos/Dreamacro/clash/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+    sudo version=$(curl -s "https://api.github.com/repos/MetaCubeX/mihomo/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+    #sudo wget --verbose --show-progress=on --progress=bar --hsts-file=/tmp/wget-hsts --continue --retry-connrefused --waitretry=1 --timeout=30 --tries=3 "https://github.com/Dreamacro/clash/releases/download/$version/clash-linux-amd64-$version.gz" -O clash.gz
+    sudo wget --verbose --show-progress=on --progress=bar --hsts-file=/tmp/wget-hsts --continue --retry-connrefused --waitretry=1 --timeout=30 --tries=3 "https://github.com/MetaCubeX/mihomo/releases/download/$version/mihomo-linux-amd64-$version.gz" -O mihomo.gz
 else
     echo "is not x86_64"
 fi
@@ -18,10 +21,10 @@ unset http_proxy https_proxy all_proxy HTTP_PROXY HTTPS_PROXY ALL_PROXY
 
 gunzip -f mihomo.gz
 
-mv -fv ./mihomo ./mihomos
-cp -fv ./mihomos ../sfs/sources/linux-amd64
-chmod -v +x ./mihomos ../sfs/sources/linux-amd64
-rm -fv ./mihomo.gz ./mihomo
+sudo mv -fv ./mihomo ./mihomos
+sudo cp -fv ./mihomos ../sfs/sources/linux-amd64
+sudo chmod -v +x ./mihomos ../sfs/sources/linux-amd64
+sudo rm -fv ./mihomo.gz ./mihomo
 
 # Initialize Mihomo
 sudo chmod +x ./mihomos && ./mihomos &
@@ -31,5 +34,5 @@ sudo chmod 777 /etc/proxychains.conf
 sudo cp -fv ./utils/scripts/proxychains.conf /etc/proxychains.conf
 
 # Run Mihomo
-pkill -f mihomos
-./mihomos -f ./utils/scripts/clash_config.yml &
+sudo pkill -f mihomos
+sudo ./mihomos -f ./utils/scripts/clash_config.yml &
